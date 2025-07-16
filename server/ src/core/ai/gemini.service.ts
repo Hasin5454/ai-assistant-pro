b2d456fa2c1530b59c5a79cc3d5751a29c69b687
Minarul Pro coders <fahimaaktarrajahmed@gmail.com>
2025-07-16 19:14:46 +0600

    Update gemini.service.ts

--- server/ src/core/ai/gemini.service.ts
+++ server/ src/core/ai/gemini.service.ts
@@ -1,1 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
+@Injectable()
+export class GeminiService {
+  private ai: GoogleGenerativeAI;
+  private model: string;
+
+  constructor() {
+    this.ai = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+    this.model = 'gemini-1.5-pro';
+  }
+
+  async generateContent(prompt: string) {
+    const model = this.ai.getGenerativeModel({ 
+      model: this.model,
+      generationConfig: {
+        maxOutputTokens: 2048,
+      }
+    });
+    
+    const result = await model.generateContent(prompt);
+    return result.response.text();
+  }
+}
