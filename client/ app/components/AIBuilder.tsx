48efbbe7ab2830763ef01121b08d2fec77e4a46d
Minarul Pro coders <fahimaaktarrajahmed@gmail.com>
2025-07-16 19:13:33 +0600

    Update AIBuilder.tsx

--- client/ app/components/AIBuilder.tsx
+++ client/ app/components/AIBuilder.tsx
@@ -1,1 +1,32 @@
+'use client';
+import { useAIBuilder } from '@/hooks/useAIBuilder';
 
+export function AIBuilder() {
+  const {
+    components,
+    addComponent,
+    generateCode,
+    previewUrl
+  } = useAIBuilder();
+
+  return (
+    <div className="aibuilder-grid">
+      <ComponentPalette onSelect={addComponent} />
+      
+      <CanvasArea>
+        {components.map((comp) => (
+          <DraggableComponent key={comp.id} {...comp} />
+        ))}
+      </CanvasArea>
+      
+      <PreviewPanel url={previewUrl} />
+      
+      <button 
+        onClick={generateCode}
+        className="generate-btn"
+      >
+        Generate Code
+      </button>
+    </div>
+  );
+}
