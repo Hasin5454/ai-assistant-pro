a06e68151e787e4b9c4e248ddac61ca921464574
Minarul Pro coders <fahimaaktarrajahmed@gmail.com>
2025-07-16 19:15:44 +0600

    Update design.service.ts

--- server/ src/modules/design/design.service.ts
+++ server/ src/modules/design/design.service.ts
@@ -1,1 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import * as Figma from 'figma-api';
+import { HeroUIService } from './hero-ui.service';
 
+@Injectable()
+export class DesignService {
+  constructor(
+    private readonly figma: Figma,
+    private readonly heroUI: HeroUIService
+  ) {}
+
+  async generateFromFigma(url: string) {
+    const figmaDesign = await this.figma.extractComponents(url);
+    return this.heroUI.convertToCode(figmaDesign);
+  }
+
+  async generateFromHeroUI(componentName: string) {
+    return this.heroUI.getComponent(componentName);
+  }
+}
